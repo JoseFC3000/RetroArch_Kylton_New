c7dcf5a9eaaf2755e193f96fdee787a918f616d4
JoseFC3000 <70484443+JoseFC3000@users.noreply.github.com>
2025-05-12 06:39:07 +0100

    Update menu_cbs_left.c

--- menu/cbs/menu_cbs_left.c
+++ menu/cbs/menu_cbs_left.c
@@ -951,8 +951,8 @@ static int action_left_state_slot(unsigned type, const char *label,
    settings_t       *settings = config_get_ptr();
 
    settings->ints.state_slot--;
-   if (settings->ints.state_slot < -1)
-      settings->ints.state_slot = 999;
+   if (settings->ints.state_slot < 1)
+      settings->ints.state_slot = 1;
 
    if (menu_st->driver_ctx)
    {
